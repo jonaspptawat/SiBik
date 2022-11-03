6d68aada5abc4eb474a10551efb5e7f1c8a6ba86
jonaspptawat <jonaspptawat@gmail.com>
2022-11-03 10:28:43 +0700

    Add transformation config

--- detector/config/defaults.py
+++ detector/config/defaults.py
@@ -32,4 +32,12 @@ _C.DATA.HEIGHT = 416
 # -----------------------------------------------------------------------------
 # Transformation
 # -----------------------------------------------------------------------------
-
+_C.T = CN()
+# Horizontal Flip Prob
+_C.T.PROB_HFLIP = 0.4
+# RandomBrightness and Contrast config
+_C.T.BRIGHT_JITTER = 0.2
+_C.T.CONTRAST_JITTER = 0.2
+_C.T.PROB_BRIGHTCONTRAST = 0.1
+# Bounding box min visibility
+_C.T.BBOX_MINVIS = 0.55
