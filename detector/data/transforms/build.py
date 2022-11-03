981ebe8c1fb2fdffdaf2967e7c4fdee23924a0dc
jonaspptawat <jonaspptawat@gmail.com>
2022-11-03 14:29:27 +0700

    Add reshape and padding module

--- detector/data/transforms/build.py
+++ detector/data/transforms/build.py
@@ -1,5 +1,6 @@
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
+import cv2
 
 def build_transforms(cfg, is_train=True):
 
@@ -7,6 +8,11 @@ def build_transforms(cfg, is_train=True):
 
         transform = A.Compose([
             A.LongestMaxSize(max_size=int(cfg.DATA.WIDTH)),
+            A.PadIfNeeded(
+                min_height=int(cfg.DATA.HEIGHT),
+                min_width=int(cfg.DATA.WIDTH),
+                border_mode=cv2.BORDER_CONSTANT
+                ),
             A.HorizontalFlip(p=cfg.T.PROB_HFLIP),
             A.RandomBrightnessContrast(brightness_limit=cfg.T.BRIGHT_JITTER,
                                        contrast_limit=cfg.T.CONTRAST_JITTER,
@@ -18,6 +24,11 @@ def build_transforms(cfg, is_train=True):
         
         transform = A.Compose([
             A.LongestMaxSize(max_size=int(cfg.DATA.WIDTH)),
+            A.PadIfNeeded(
+                min_height=int(cfg.DATA.HEIGHT),
+                min_width=int(cfg.DATA.WIDTH),
+                border_mode=cv2.BORDER_CONSTANT
+                ),
             A.Normalize(),
             ToTensorV2()
             ], bbox_params=A.BboxParams(format='yolo', min_visibility=cfg.T.BBOX_MINVIS, label_fields=[]))
