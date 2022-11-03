0432a306ccae4541955494e4a8a54f7fde7da20d
jonaspptawat <jonaspptawat@gmail.com>
2022-11-03 13:37:47 +0700

    Add Checkpoint and Solver config

--- detector/config/defaults.py
+++ detector/config/defaults.py
@@ -56,3 +56,33 @@ _C.TEST = CN()
 _C.TEST.CONF_THRESH = 0.85
 _C.TEST.NMS_THRESH = 0.005
 _C.TEST.MAP_THRESH = 0.85
+
+# -----------------------------------------------------------------------------
+# Solver
+# -----------------------------------------------------------------------------
+_C.SOLVER = CN()
+_C.SOLVER.OPTIMIZER = "SGD"
+
+_C.SOLVER.EPOCHS = 1000
+
+_C.SOLVER.BASE_LR = 0.001
+
+_C.SOLVER.MOMENTUM = 0.9
+
+_C.SOLVER.WEIGHT_DECAY = 0.0005
+
+_C.SOLVER.GAMMA = 0.1
+_C.SOLVER.STEPS = (150, 300, 450, 600)
+
+_C.SOLVER.WARMUP_EXPO = 1.0 / 3
+_C.SOLVER.WARMUP_ITERS_TIMES = 5
+
+_C.SOLVER.CHECKPOINT_PERIOI = 1
+_C.SOLVER.LOG_PERIOD = 1
+
+# -----------------------------------------------------------------------------
+# Checkpoint
+# -----------------------------------------------------------------------------
+_C.CHECKPOINT = CN()
+
+_C.CHECKPOINT_PATH = "./checkpoint/check.pth"
