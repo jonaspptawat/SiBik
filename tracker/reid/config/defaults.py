91cbc57a6dabe44d9d4752132d4f19b06133c07d
jonaspptawat <jonaspptawat@gmail.com>
2022-11-05 13:14:43 +0700

    Add input transformation config

--- tracker/reid/config/defaults.py
+++ tracker/reid/config/defaults.py
@@ -27,3 +27,31 @@ _C.DATA.VAL = "./data/datasets/cars/val"
 # Batch size
 _C.DATA.SIZE_TRAIN = 16
 _C.DATA.SIZE_VAL = 1
+# Class config
+_C.DATA.CLASS_SEP = 25
+
+# -----------------------------------------------------------------------------
+# INPUT TRANSFORMATION
+# -----------------------------------------------------------------------------
+_C.INPUT = CN()
+# Scale of Image resizing
+_C.INPUT.RESIZE_SCALE = 1.1
+# Brightness jittering
+_C.INPUT.BRIGHT_JITTER = 0.25
+# Contrast jittering
+_C.INPUT.CONTRAST_JITTER = 0.25
+# Saturation jittering
+_C.INPUT.SAT_JITTER = 0.25
+# Hue jittering
+_C.INPUT.HUE_JITTER = 0.25
+# Jitter Probability
+_C.INPUT.PROB_JITTER = 0.5
+# Horizontal Flip Probability
+_C.INPUT.PROB_HOR = 0.5
+# Blur Probability
+_C.INPUT.PROB_BLUR = 0.45
+# To gray Probability
+_C.INPUT.PROB_TOGRAY = 0.05
+# Coarse Dropout Probability
+_C.INPUT.PROB_CDROP = 0.55
+
